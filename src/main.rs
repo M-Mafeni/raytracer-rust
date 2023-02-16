806ba37e96ba49e67d9e4194c90215490a2ff985
M-Mafeni <mafeni99@gmail.com>
2023-02-16 00:21:24 +0000

    Add Vector module and ppm write function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use crate::vector::vector::{Vec3, dot_product, cross_product};
+
+mod vector;
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH: i16 = 256;
+    const IMAGE_HEIGHT: i16 = 256;
+
+    let v1 = Vec3::new(4.0, 5.0, 6.0);
+    let v2 = Vec3::new(8.0, 7.0, 10.0);
+
+    println!("v1 + v2 is {:?}", v1 + v2);
+    println!("v1 - v2 is {:?}", v1 - v2);
+    println!("v2 - v1 is {:?}", v2 - v1);
+    println!("v1 * v2 is {:?}", v1 * v2);
+    println!("v1 * 5 is {:?}", v1 * 5.0);
+    println!("5 * v1 is {:?}", 5.0 * v1);
+    println!("length of v1 is {}", v1.length());
+    println!("unit vector of v1 is {:?}", v1.unit_vector());
+    println!("v1.v2 is {:?}", dot_product(v1, v2));
+    println!("v1 X v2 is {:?}", cross_product(v1, v2));
+
+
+    /* Uncomment for creating ppm image
+    
+        println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+        for j in (0..IMAGE_HEIGHT).rev() {
+            eprint!("\rScanlines Remaining: {} ", j);
+            for i in 0..IMAGE_WIDTH {
+                let r: f32 = i as f32 / (IMAGE_WIDTH - 1) as f32;
+                let g: f32 = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+                let b: f32 = 0.25;
+
+                let ir: u16 = (255.999 * r) as u16;
+                let ig: u16 = (255.999 * g) as u16;
+                let ib: u16 = (255.999 * b) as u16;
+                println!("{} {} {}", ir, ig, ib);
+            }
+        }
+        eprintln!("\nDone.");
+    
+     */
 }
---
+++ src/vector.rs
@@ -0,0 +1,139 @@
+pub mod vector {
+    use std::ops;
+
+    pub fn vec3() -> Vec3 {
+        Vec3 {
+            points: [0.0, 0.0, 0.0]
+        }
+    }
+
+
+    #[derive(Debug, Clone, Copy)]
+    pub struct Vec3 {
+        points: [f64; 3]
+    }
+
+    impl Vec3 {
+
+        pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
+            Vec3 {
+                points: [e0, e1, e2]
+            }
+        }
+
+        fn x(&self) -> f64 {
+            self.points[0]
+        }
+
+        fn y(&self) -> f64 {
+            self.points[1]
+        }
+
+        fn z(&self) -> f64 {
+            self.points[2]
+        }
+
+        fn scale(&self, t: f64) -> Vec3 {
+            let Vec3 { points: p } = self;
+            Vec3 { points: [p[0] * t, p[1] * t, p[2] * t] }
+        }
+
+        pub fn length(&self) -> f64 {
+            let Vec3 { points } = self;
+            let t = points[0] * points[0] + points[1] * points[1] + points[2] * points[2];
+            t.sqrt()
+        }
+
+        pub fn unit_vector(&self) -> Vec3 {
+            self.scale(1.0 / self.length())
+        }
+    }
+
+    impl ops::Add<Vec3> for Vec3 {
+        type Output = Vec3;
+        fn add(self, rhs: Vec3) -> Self::Output {
+            let Vec3{points: p1} = self;
+            let Vec3{points: p2} = rhs;
+            Vec3 {
+                points: [
+                    p1[0] + p2[0],
+                    p1[1] + p2[1],
+                    p1[2] + p2[2],
+                ]
+            }
+        }
+    }
+
+    impl ops::Sub<Vec3> for Vec3 {
+        type Output = Vec3;
+        fn sub(self, rhs: Vec3) -> Self::Output {
+            let Vec3{points: p1} = self;
+            let Vec3{points: p2} = rhs;
+            Vec3 {
+                points: [
+                    p1[0] - p2[0],
+                    p1[1] - p2[1],
+                    p1[2] - p2[2],
+                ]
+            }
+        }
+    }
+
+    impl ops::Mul<Vec3> for Vec3 {
+        type Output = Vec3;
+        fn mul(self, rhs: Vec3) -> Self::Output {
+            let Vec3{points: p1} = self;
+            let Vec3{points: p2} = rhs;
+            Vec3 {
+                points: [
+                    p1[0] * p2[0],
+                    p1[1] * p2[1],
+                    p1[2] * p2[2],
+                ]
+            }
+        }
+    }
+
+    impl ops::Mul<f64> for Vec3 {
+        type Output = Vec3;
+
+        fn mul(self, rhs: f64) -> Self::Output {
+            self.scale(rhs)
+        }
+    }
+
+    impl ops::Mul<Vec3> for f64 {
+        type Output = Vec3;
+
+        fn mul(self, rhs: Vec3) -> Self::Output {
+            rhs.scale(self)
+        }
+    }
+
+    pub type Point3 = Vec3;
+    pub type Color = Vec3;
+
+    pub fn color(r: u16, g: u16, b: u16) -> Vec3 {
+        Vec3 { points: [f64::from(r), f64::from(g), f64::from(b)] }
+    }
+
+    // utility functions
+
+    pub fn dot_product(u: Vec3, v: Vec3) -> f64 {
+        let Vec3{points: p1} = u;
+        let Vec3{points: p2} = v;
+        p1[0] * p2[0] + p1[1] * p2[1] + p1[2] * p2[2]
+    }
+
+    pub fn cross_product(u: Vec3, v: Vec3) -> Vec3 {
+        let Vec3{points: p1} = u;
+        let Vec3{points: p2} = v;
+        Vec3 {
+            points: [
+                p1[1] * p2[2] - p1[2] * p2[1],
+                p1[2] * p2[0] - p1[0] * p2[2],
+                p1[0] * p2[1] - p1[1] * p2[0]
+            ]
+        }
+    }
+}
