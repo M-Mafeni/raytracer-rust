1f0829a5fb8c913cbd40a15228a24d3cbfe86879
M-Mafeni <mafeni99@gmail.com>
2023-02-18 23:44:04 +0000

    Render normals-colored sphere with ground

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::f64::INFINITY;
 use std::fs::File;
 use std::io::{Write};
 
+use hittable::hittable::Hittable;
 use vector::vector::{Color, Point3, unit_vector};
 
+use crate::hittable::hittable::{HittableList, create_new_hittable_list};
 use crate::ray::ray::{Ray, ray};
+use crate::shapes::sphere::sphere;
 use crate::vector::vector::{Vec3, dot_product, vec3, zero_vector};
 use crate::writer::writer::write_color;
 
@@ -27,11 +31,9 @@ fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     }
 }
 
-fn ray_color(ray: Ray) -> Color {
-    let hit_value = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, &ray);
-    if hit_value > 0.0 {
-        let n = unit_vector(ray.at(hit_value) - vec3(0.0, 0.0, -1.0));
-        return 0.5 * vec3(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0)
+fn ray_color<T: Hittable>(ray: Ray, world: &HittableList<T>) -> Color {
+    if let Some(hit_record) = world.hit(&ray, 0.0, INFINITY) {
+        return 0.5 * (hit_record.normal + vec3(1.0, 1.0, 1.0))
     }
     let unit_dir = ray.direction().unit_vector();
     let t = 0.5 * (unit_dir.y() + 1.0);
@@ -42,6 +44,11 @@ fn main() -> std::io::Result<()>{
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u16 = 400;
     const IMAGE_HEIGHT: u16 = (IMAGE_WIDTH as f32 / ASPECT_RATIO as f32) as u16;
+
+    // World
+    let mut world: HittableList<_> = create_new_hittable_list();
+    world.objects.push(sphere(vec3(0.0, 0.0, -1.0), 0.5));
+    world.objects.push(sphere(vec3(0.0, -100.5, -1.0), 100.0));
 
     // Camera
     let viewport_height = 2.0;
@@ -62,7 +69,7 @@ fn main() -> std::io::Result<()>{
             let u = f64::from(i) / f64::from(IMAGE_WIDTH - 1);
             let v = f64::from(j) / f64::from(IMAGE_HEIGHT - 1);
             let r = ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            let pixel_color = ray_color(r);
+            let pixel_color = ray_color(r, &world);
             write_color(&buffer, pixel_color)?
         }
     }
