963faeb34082b883196c7fe47dff9b7f6265f87a
M-Mafeni <mafeni99@gmail.com>
2023-02-19 13:22:51 +0000

    Add random generator class

---
+++ src/random.rs
@@ -0,0 +1,14 @@
+pub mod random {
+    use rand::Rng;
+
+    // returns a double between 0 and 1
+    pub fn random_double() -> f64 {
+        let mut rng = rand::thread_rng();
+        rng.gen::<f64>()
+    }
+
+    pub fn random_double_range(min: f64, max: f64) -> f64 {
+        min + (max-min)*random_double()
+    }
+
+}
