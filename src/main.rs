e24b7bcf38cfb191ba823a14134b0d4f6fb37ebd
M-Mafeni <mafeni99@gmail.com>
2023-02-19 20:20:02 +0000

    Remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::{Write};
 
 use hittable::Hittable;
 use ray::ray;
-use vector::{Color, random_in_unit_sphere, random_unit_vector, random_in_hemisphere};
+use vector::{Color, random_in_unit_sphere};
 
 use crate::camera::initialise_camera;
 use crate::hittable::{HittableList, create_new_hittable_list};
