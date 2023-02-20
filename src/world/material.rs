aa9a162e0dea2f30bbfd483730b08bc31055d91b
M-Mafeni <mafeni99@gmail.com>
2023-02-20 20:28:44 +0000

    Make Material an Enum

--- src/world/material.rs
+++ src/world/material.rs
@@ -1,11 +1,26 @@
-use crate::{ray::Ray, vector::{Color, Point3}};
+use crate::{ray::{Ray, ray}, vector::{Color, Point3, random_unit_vector}};
 
-pub mod lambertian;
+pub enum Material {
+    Lambertian {albedo: Color}
+}
+
 pub struct ScatterResult {
     pub attenuation: Color,
     pub scattered: Ray
 }
 
-pub trait Material {
+pub trait Scatter {
     fn scatter(&self, r: &Ray, p: Point3, normal: Point3, t: f64) -> Option<ScatterResult>;
+}
+
+impl Scatter for Material {
+    fn scatter(&self, r: &Ray, p: Point3, normal: Point3, t: f64) -> Option<ScatterResult> {
+        match self {
+            Material::Lambertian {albedo} => {
+                let scatter_direction = normal + random_unit_vector();
+                let scattered = ray(p, scatter_direction);
+                Some(ScatterResult { attenuation: *albedo, scattered })
+            }
+        }
+    }
 }
